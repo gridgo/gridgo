d1c2e4cbb797b68d8376070e4e620474b9f9cc62
Anh Dung Bui <griever@10.100.203.62>
2018-12-27 09:48:48 +0700

    move examples to another repo

--- examples/gridgo-example-tik-tac-toe/src/main/java/io/gridgo/example/tiktactoe/game/GameEventType.java
+++
@@ -1,6 +0,0 @@
-package io.gridgo.example.tiktactoe.game;
-
-public enum GameEventType {
-
-    PLAYER_JOIN, PLAYER_QUIT, PLAYER_MOVE, GAME_DELETED;
-}
--- examples/gridgo-example-tik-tac-toe/src/main/java/io/gridgo/example/tiktactoe/user/UserEventType.java
+++
@@ -1,6 +0,0 @@
-package io.gridgo.example.tiktactoe.user;
-
-public enum UserEventType {
-
-    LOGGED_IN, LOGGED_OUT, REQUEST;
-}
