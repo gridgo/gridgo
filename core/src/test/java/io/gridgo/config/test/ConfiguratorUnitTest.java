50449d8221af29462f475f295825c87691a7fefa
Dung Bui <griever@192.168.1.30>
2019-01-08 15:39:12 +0700

    add configuration unit test

--- core/src/test/java/io/gridgo/config/test/ConfiguratorUnitTest.java
+++ core/src/test/java/io/gridgo/config/test/ConfiguratorUnitTest.java
@@ -1,4 +1,4 @@
-package io.gridgo.core.test;
+package io.gridgo.config.test;
 
 import org.junit.Assert;
 import org.junit.Test;
---
+++ core/src/test/java/io/gridgo/config/test/TestProcessor.java
@@ -0,0 +1,13 @@
+package io.gridgo.config.test;
+
+import io.gridgo.core.GridgoContext;
+import io.gridgo.core.Processor;
+import io.gridgo.core.support.RoutingContext;
+
+public class TestProcessor implements Processor {
+
+    @Override
+    public void process(RoutingContext rc, GridgoContext gc) {
+        
+    }
+}
