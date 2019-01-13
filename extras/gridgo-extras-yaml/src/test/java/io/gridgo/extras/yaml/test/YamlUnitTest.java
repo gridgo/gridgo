e4e724d983038b2908f33e2daaf34ddff16c9924
Anh Dung Bui <griever@192.168.1.34>
2019-01-13 23:40:35 +0700

    add unit test

--- extras/gridgo-extras-yaml/src/test/java/io/gridgo/extras/yaml/test/YamlUnitTest.java
+++ extras/gridgo-extras-yaml/src/test/java/io/gridgo/extras/yaml/test/YamlUnitTest.java
@@ -1,5 +1,9 @@
 package io.gridgo.extras.yaml.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,8 +12,20 @@ import io.gridgo.extras.yaml.YamlConfigurator;
 public class YamlUnitTest {
 
     @Test
-    public void testYaml() {
+    public void testYamlResource() {
         var yaml = YamlConfigurator.ofResource("test.yml");
+        yaml.start();
+        var config = yaml.get().orElseThrow().asObject();
+        Assert.assertEquals("test", config.getString("applicationName"));
+        Assert.assertNotNull(config.getObject("gateways"));
+        Assert.assertNotNull(config.getObject("gateways").getObject("test"));
+        Assert.assertFalse(config.getObject("gateways").getObject("test").getArray("subscribers").isEmpty());
+    }
+
+    @Test
+    public void testYamlFile() throws URISyntaxException, FileNotFoundException {
+        var resource = getClass().getClassLoader().getResource("test.yml").toURI();
+        var yaml = YamlConfigurator.ofFile(new File(resource));
         yaml.start();
         var config = yaml.get().orElseThrow().asObject();
         Assert.assertEquals("test", config.getString("applicationName"));
