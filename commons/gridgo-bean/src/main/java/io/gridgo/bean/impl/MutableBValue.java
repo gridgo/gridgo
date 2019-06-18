92d19bd4cbe3bdd953463506d459483f829e5db0
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-06-18 17:02:13 +0700

    [bean] update hash code for bvalue

--- commons/gridgo-bean/src/main/java/io/gridgo/bean/impl/MutableBValue.java
+++ commons/gridgo-bean/src/main/java/io/gridgo/bean/impl/MutableBValue.java
@@ -6,6 +6,7 @@ import io.gridgo.bean.BValue;
 import io.gridgo.bean.exceptions.InvalidTypeException;
 import io.gridgo.bean.serialization.text.BPrinter;
 import io.gridgo.utils.PrimitiveUtils;
+import io.gridgo.utils.hash.BinaryHashCodeCalculator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,57 +14,70 @@ import lombok.Setter;
 @NoArgsConstructor
 public class MutableBValue extends AbstractBElement implements BValue {
 
-    @Setter
-    @Getter
-    private Object data;
+	private transient static final BinaryHashCodeCalculator binaryHashCodeCalculator = BinaryHashCodeCalculator.XXHASH32_JAVA_SAFE;
 
-    public MutableBValue(Object data) {
-        if (data != null && !(data instanceof byte[]) && !PrimitiveUtils.isPrimitive(data.getClass())) {
-            throw new InvalidTypeException("Cannot create DefaultBValue from: " + data.getClass() + " instance");
-        }
-        this.setData(data);
-    }
+	@Setter
+	@Getter
+	private Object data;
 
-    @Override
-    public String toString() {
-        StringBuilder writer = new StringBuilder();
-        BPrinter.print(writer, this);
-        return writer.toString();
-    }
+	public MutableBValue(Object data) {
+		if (data != null && !(data instanceof byte[]) && !PrimitiveUtils.isPrimitive(data.getClass())) {
+			throw new InvalidTypeException("Cannot create DefaultBValue from: " + data.getClass() + " instance");
+		}
+		this.setData(data);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        var otherData = obj;
-        if (obj instanceof BValue) {
-            otherData = ((BValue) obj).getData();
-        }
+	@Override
+	public String toString() {
+		StringBuilder writer = new StringBuilder();
+		BPrinter.print(writer, this);
+		return writer.toString();
+	}
 
-        if (data == null)
-            return otherData == null;
+	@Override
+	public boolean equals(Object obj) {
+		var otherData = obj;
+		if (obj instanceof BValue) {
+			otherData = ((BValue) obj).getData();
+		}
 
-        if (otherData == null)
-            return false;
+		if (data == null)
+			return otherData == null;
 
-        if (data == otherData || data.equals(otherData))
-            return true;
+		if (otherData == null)
+			return false;
 
-        if (data instanceof Number && otherData instanceof Number)
-            return ((Number) data).doubleValue() == ((Number) otherData).doubleValue();
+		if (data == otherData || data.equals(otherData))
+			return true;
 
-        if (data instanceof String && otherData instanceof Character)
-            return data.equals(String.valueOf((Character) otherData));
+		if (data instanceof Number && otherData instanceof Number)
+			return ((Number) data).doubleValue() == ((Number) otherData).doubleValue();
 
-        if (data instanceof Character && otherData instanceof String)
-            return otherData.equals(String.valueOf((Character) data));
+		if (data instanceof String && otherData instanceof Character)
+			return data.equals(String.valueOf((Character) otherData));
 
-        if (data instanceof byte[] && otherData instanceof byte[])
-            return Arrays.equals((byte[]) data, (byte[]) otherData);
+		if (data instanceof Character && otherData instanceof String)
+			return otherData.equals(String.valueOf((Character) data));
 
-        return false;
-    }
+		if (data instanceof byte[] && otherData instanceof byte[])
+			return Arrays.equals((byte[]) data, (byte[]) otherData);
 
-    @Override
-    public int hashCode() {
-        return data != null ? data.hashCode() : super.hashCode();
-    }
+		return false;
+	}
+
+	/**
+	 * Optimized hash code calculating for binary
+	 */
+	@Override
+	public int hashCode() {
+		if (data == null) {
+			return super.hashCode();
+		}
+
+		if (data instanceof byte[]) {
+			return binaryHashCodeCalculator.calcHashCode((byte[]) data);
+		}
+
+		return data.hashCode();
+	}
 }
---
+++ commons/gridgo-bean/src/test/java/io/gridgo/bean/test/TestBValueEqualsAndHashCode.java
@@ -0,0 +1,79 @@
+package io.gridgo.bean.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import io.gridgo.bean.BValue;
+import io.gridgo.utils.hash.BinaryHashCodeCalculator;
+
+public class TestBValueEqualsAndHashCode {
+
+	// @Test
+	public void benchmarkHashCode()
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		List<String> list = new LinkedList<>();
+		for (int i = 0; i < 100000; i++) {
+			list.add("alsdhfla snfoiwufo sdoifhasp faofh oaish ashf" + i);
+		}
+
+		// using default String.hashCode()
+		long startTime = System.nanoTime();
+		for (String str : list) {
+			str.hashCode();
+		}
+		long time1 = System.nanoTime() - startTime;
+
+		// using XXHASH32
+		startTime = System.nanoTime();
+		for (String str : list) {
+			BinaryHashCodeCalculator.XXHASH32_JNI.calcHashCode(str.getBytes());
+		}
+		long time2 = System.nanoTime() - startTime;
+
+		// optimize using reflection
+		var valueField = String.class.getDeclaredField("value");
+		valueField.setAccessible(true);
+		startTime = System.nanoTime();
+		for (String str : list) {
+			BinaryHashCodeCalculator.XXHASH32_JNI.calcHashCode((byte[]) valueField.get(str));
+		}
+		long time3 = System.nanoTime() - startTime;
+
+		System.out.println("time1 = " + time1);
+		System.out.println("time2 = " + time2);
+		System.out.println("time3 = " + time3);
+
+		System.out.println("time2 - time1 = " + (time2 - time1));
+		System.out.println("time3 - time1 = " + (time3 - time1));
+	}
+
+	@Test
+	public void testEqualsAndHashCode() {
+		Map<BValue, Object> map = new HashMap<>();
+		map.put(BValue.of(true), "true");
+		map.put(BValue.of(false), "false");
+		map.put(BValue.of((int) 1), "int");
+		map.put(BValue.of((float) 1.0), "float");
+		map.put(BValue.of('a'), "char");
+
+		map.put(BValue.of("str"), "str");
+		map.put(BValue.of(new byte[] { 0, 1, 2, 3, 4, 5 }), "raw");
+
+		assertEquals("true", map.get(BValue.of(true)));
+		assertEquals("false", map.get(BValue.of(false)));
+
+		assertEquals("int", map.get(BValue.of((int) 1)));
+		assertEquals("float", map.get(BValue.of((float) 1.0)));
+
+		assertEquals("char", map.get(BValue.of(BValue.of('a'))));
+		assertEquals("str", map.get(BValue.of("str")));
+
+		assertEquals("raw", map.get(BValue.of(new byte[] { 0, 1, 2, 3, 4, 5 })));
+	}
+}
