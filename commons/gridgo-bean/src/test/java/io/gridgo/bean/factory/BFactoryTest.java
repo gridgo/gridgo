8dfcac578b166734d97e6e0e7edf325efa714a3c
Tran Nguyen Ban <ban.tran@tiki.vn>
2019-10-16 18:18:53 +0700

    add some test and fix for run all test well

---
+++ commons/gridgo-bean/src/test/java/io/gridgo/bean/factory/BFactoryTest.java
@@ -0,0 +1,17 @@
+package io.gridgo.bean.factory;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
