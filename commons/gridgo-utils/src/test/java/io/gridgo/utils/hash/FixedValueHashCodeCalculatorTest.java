6afda3f51ed2c0fd289d84c2cfe3c82cc5c48227
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-10-18 10:52:12 +0700

    remove warning from unit test

--- commons/gridgo-utils/src/test/java/io/gridgo/utils/hash/FixedValueHashCodeCalculatorTest.java
+++ commons/gridgo-utils/src/test/java/io/gridgo/utils/hash/FixedValueHashCodeCalculatorTest.java
@@ -9,7 +9,7 @@ public class FixedValueHashCodeCalculatorTest {
     @Test
     public void calcHashCode() {
         int val = 100;
-        var x = new FixedValueHashCodeCalculator(val);
+        var x = new FixedValueHashCodeCalculator<Object>(val);
         assertEquals(val, x.calcHashCode(new Object()));
     }
 }
