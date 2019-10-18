c6c341088111ceb5b1bc574bbe7c05f2b83cbf85
Dzung Bui <dungba.sg@gmail.com>
2019-10-18 13:20:48 +0900

    Dungba/developing (#58)
    * move unit test
    
    * add unit tests
    
    * add unit tests
    
    * add unit tests
    
    * add unit tests for ArrayUtils

--- commons/gridgo-utils/src/test/java/io/gridgo/utils/test/ArrayUtilsUnitTest.java
+++ commons/gridgo-utils/src/test/java/io/gridgo/utils/test/ArrayUtilsUnitTest.java
@@ -1,13 +1,13 @@
 package io.gridgo.utils.test;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import org.junit.Assert;
-import org.junit.Test;
 
 import io.gridgo.utils.ArrayUtils;
 
@@ -24,11 +24,34 @@ public class ArrayUtilsUnitTest {
     }
 
     @Test
-    public void testForEach() {
+    public void testForEachInteger() {
         var atomic = new AtomicInteger(0);
         var list = new Integer[] { 1, 2, 3, 4 };
         ArrayUtils.<Integer>foreach(list, e -> atomic.addAndGet(e));
         Assert.assertEquals(10, atomic.get());
+    }
+
+    @Test
+    public void testForEachInt() {
+        var atomic = new AtomicInteger(0);
+        var list = new int[] { 1, 2, 3, 4 };
+        ArrayUtils.<Integer>foreach(list, e -> atomic.addAndGet(e));
+        Assert.assertEquals(10, atomic.get());
+    }
+
+    @Test
+    public void testForEachBool() {
+        var trueCounter = new AtomicInteger(0);
+        var falseCounter = new AtomicInteger(0);
+        var list = new boolean[] { true, false, true, false };
+        ArrayUtils.<Boolean>foreach(list, e -> {
+            if (e)
+                trueCounter.addAndGet(1);
+            else
+                falseCounter.addAndGet(1);
+        });
+        Assert.assertEquals(2, trueCounter.get());
+        Assert.assertEquals(2, falseCounter.get());
     }
 
     @Test
@@ -46,5 +69,15 @@ public class ArrayUtilsUnitTest {
         var arr = new Integer[] { 1, 2, 3, 4 };
         var list2 = Arrays.asList(new Integer[] { 1, 2, 3, 4 });
         Assert.assertArrayEquals(arr, ArrayUtils.toArray(Integer.class, list2));
+    }
+
+    @Test
+    public void testToString() {
+        var arr1 = new Integer[] { 1, 2, 3, 4 };
+        Assert.assertEquals("1, 2, 3, 4", ArrayUtils.toString(arr1));
+        var arr2 = new int[] { 1, 2, 3, 4 };
+        Assert.assertEquals("1, 2, 3, 4", ArrayUtils.toString(arr2));
+        var arr3 = new boolean[] { true, false, true, false };
+        Assert.assertEquals("true, false, true, false", ArrayUtils.toString(arr3));
     }
 }
