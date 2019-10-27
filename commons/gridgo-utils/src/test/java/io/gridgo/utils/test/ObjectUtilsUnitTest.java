ff089a0ce9ddce1fb2929e085348a8adfb17e01c
Anh Dung Bui <buidun@amazon.com>
2019-10-27 18:17:20 +0900

    fix unit test

--- commons/gridgo-utils/src/test/java/io/gridgo/utils/test/ObjectUtilsUnitTest.java
+++ commons/gridgo-utils/src/test/java/io/gridgo/utils/test/ObjectUtilsUnitTest.java
@@ -19,9 +19,9 @@ public class ObjectUtilsUnitTest {
         map.put("testStr", "hello");
         map.put("testArr", new int[] { 1, 2, 3 });
 
-        var innerMap = new HashMap<String, Object>();
-        innerMap.put("testInt", 2);
-        map.put("testObj", innerMap);
+        var innerObj = new TestObject();
+        innerObj.setTestInt(2);
+        map.put("testObj", innerObj);
 
         var obj = new TestObject();
         ObjectUtils.assembleFromMap(TestObject.class, obj, map);
@@ -30,6 +30,11 @@ public class ObjectUtilsUnitTest {
         Assert.assertEquals("hello", obj.getTestStr());
         Assert.assertEquals(2, obj.getTestObj().getTestInt());
 
+        var innerMap = new HashMap<String, Object>();
+        innerMap.put("testInt", 2);
         obj.setTestMap(innerMap);
+
+        int x = ObjectUtils.<Integer>getValueByPath(obj, "testObj.testInt");
+        Assert.assertEquals(2, x);
     }
 }
