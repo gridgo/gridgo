88b658d694b6194ac2d8b5c392a2b5d32eedb7ed
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-10-31 11:55:52 +0700

    update test case

--- commons/gridgo-pojo/src/test/java/io/gridgo/format/test/TestFormatTransformation.java
+++ commons/gridgo-pojo/src/test/java/io/gridgo/format/test/TestFormatTransformation.java
@@ -16,11 +16,7 @@ public class TestFormatTransformation {
         String str = "My name is {{ name > nameTransform }}, " //
                 + "{{ age }} years old, " //
                 + "monthly salary {{ salary > decrement10% > decrement50 > thousandSeparate}} {{currency > upperCase}}, " //
-                + "health {{health > percentage}}, " //
-                + "date: {{today > localOnlyDate}}, " //
-                + "time: {{today > localOnlyTime12}}, " //
-                + "fulltime: {{today > localFullTime24}}, " //
-                + "fulltime gmt: {{today > gmtFullTime12}}";
+                + "health {{health > percentage}}";
 
         long now = 1572345589467l; // fixed value for unit test
         var obj = new Dummy("MY_NAME", 30, 10000000.97, "VND", "0.9756", now);
@@ -30,7 +26,8 @@ public class TestFormatTransformation {
         transformerRegistry.addTransformer("decrement50", value -> getDoubleValueFrom(value) - 50);
         transformerRegistry.addAlias("nameTransform", "lowerCase > capitalize", "stripAccents");
 
-        var expectedResult = "My name is My_name, 30 years old, monthly salary 8,999,950 VND, health 97.56%, date: 2019/10/29 +07:00, time: 05:39:49.467 PM, fulltime: 2019/10/29 17:39:49.467 +07:00, fulltime gmt: 2019/10/29 10:39:49.467 AM Z";
-        assertEquals(expectedResult, transform(str, obj));
+        var expectedResult = "My name is My_name, 30 years old, monthly salary 8,999,950 VND, health 97.56%";
+        var actual = transform(str, obj);
+        assertEquals(expectedResult, actual);
     }
 }
