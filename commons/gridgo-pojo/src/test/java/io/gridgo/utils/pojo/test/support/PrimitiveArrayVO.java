d92002a20daa76d6093195d84ddbfb3a6dd241b8
Anh Dung Bui <buidun@amazon.com>
2019-11-02 14:22:19 +0900

    fix unit test

--- commons/gridgo-pojo/src/test/java/io/gridgo/utils/pojo/test/support/PrimitiveArrayVO.java
+++ commons/gridgo-pojo/src/test/java/io/gridgo/utils/pojo/test/support/PrimitiveArrayVO.java
@@ -3,11 +3,15 @@ package io.gridgo.utils.pojo.test.support;
 import java.util.List;
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PrimitiveArrayVO {
 
     private boolean[] booleanValue;
