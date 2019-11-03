ef14ad97aa906960002b882b5acdcd00ec3ea913
Anh Dung Bui <buidun@amazon.com>
2019-11-04 01:12:58 +0900

    refactor

--- commons/gridgo-framework/src/main/java/io/gridgo/framework/execution/impl/disruptor/DisruptorWorkerPoolExecutionStrategy.java
+++ commons/gridgo-framework/src/main/java/io/gridgo/framework/execution/impl/disruptor/DisruptorWorkerPoolExecutionStrategy.java
@@ -1,24 +1,18 @@
 package io.gridgo.framework.execution.impl.disruptor;
 
-import java.util.concurrent.ThreadFactory;
-
 import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.WaitStrategy;
 import com.lmax.disruptor.WorkHandler;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 
-import io.gridgo.framework.execution.ExecutionStrategy;
-import io.gridgo.framework.execution.impl.ExecutionContextEvent;
-import io.gridgo.framework.support.Message;
-import io.gridgo.framework.support.context.ExecutionContext;
-import io.gridgo.framework.support.context.impl.DefaultExecutionContext;
+import java.util.concurrent.ThreadFactory;
 
-public class DisruptorWorkerPoolExecutionStrategy<T, H> implements ExecutionStrategy {
+import io.gridgo.framework.execution.impl.ExecutionContextEvent;
+
+public class DisruptorWorkerPoolExecutionStrategy<T, H> extends AbstractDisruptorExecutionStrategy<T, H> {
 
     private static final int DEFAULT_BUFFER_SIZE = 1024;
-
-    private Disruptor<ExecutionContextEvent<T, H>> disruptor;
 
     public DisruptorWorkerPoolExecutionStrategy() {
         this(DEFAULT_BUFFER_SIZE);
@@ -55,30 +49,5 @@ public class DisruptorWorkerPoolExecutionStrategy<T, H> implements ExecutionStra
             };
         }
         this.disruptor.handleEventsWithWorkerPool(workers);
-    }
-
-    @Override
-    public void execute(Runnable runnable, Message request) {
-        ExecutionContext<T, H> context = new DefaultExecutionContext<>(t -> runnable.run());
-        execute(context);
-    }
-
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    @Override
-    public void execute(ExecutionContext context) {
-        this.disruptor.publishEvent((event, sequence) -> {
-            event.clear();
-            event.setContext(context);
-        });
-    }
-
-    @Override
-    public void start() {
-        disruptor.start();
-    }
-
-    @Override
-    public void stop() {
-        disruptor.shutdown();
     }
 }
--- commons/gridgo-framework/src/main/java/io/gridgo/framework/execution/impl/disruptor/SingleConsumerDisruptorExecutionStrategy.java
+++ commons/gridgo-framework/src/main/java/io/gridgo/framework/execution/impl/disruptor/SingleConsumerDisruptorExecutionStrategy.java
@@ -1,23 +1,17 @@
 package io.gridgo.framework.execution.impl.disruptor;
 
-import java.util.concurrent.ThreadFactory;
-
 import com.lmax.disruptor.BlockingWaitStrategy;
 import com.lmax.disruptor.WaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 
-import io.gridgo.framework.execution.ExecutionStrategy;
-import io.gridgo.framework.execution.impl.ExecutionContextEvent;
-import io.gridgo.framework.support.Message;
-import io.gridgo.framework.support.context.ExecutionContext;
-import io.gridgo.framework.support.context.impl.DefaultExecutionContext;
+import java.util.concurrent.ThreadFactory;
 
-public class SingleConsumerDisruptorExecutionStrategy<T, H> implements ExecutionStrategy {
+import io.gridgo.framework.execution.impl.ExecutionContextEvent;
+
+public class SingleConsumerDisruptorExecutionStrategy<T, H> extends AbstractDisruptorExecutionStrategy<T, H> {
 
     private static final int DEFAULT_BUFFER_SIZE = 1024;
-
-    private Disruptor<ExecutionContextEvent<T, H>> disruptor;
 
     public SingleConsumerDisruptorExecutionStrategy(ProducerType producerType) {
         this(producerType, DEFAULT_BUFFER_SIZE);
@@ -43,30 +37,5 @@ public class SingleConsumerDisruptorExecutionStrategy<T, H> implements Execution
 
     private void onEvent(ExecutionContextEvent<?, ?> event, long sequence, boolean endOfBatch) {
         event.getContext().execute();
-    }
-
-    @Override
-    public void execute(Runnable runnable, Message request) {
-        ExecutionContext<T, H> context = new DefaultExecutionContext<>(t -> runnable.run());
-        execute(context);
-    }
-
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    @Override
-    public void execute(ExecutionContext context) {
-        this.disruptor.publishEvent((event, sequence) -> {
-            event.clear();
-            event.setContext(context);
-        });
-    }
-
-    @Override
-    public void start() {
-        disruptor.start();
-    }
-
-    @Override
-    public void stop() {
-        disruptor.shutdown();
     }
 }
