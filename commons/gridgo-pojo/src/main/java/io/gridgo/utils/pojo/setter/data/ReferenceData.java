be2bdba566f683b83b9136238ddcfec09d6fbdfb
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2019-11-26 18:31:17 +0700

    rollback ReferenceData, donot inherit from KeyValueData

--- commons/gridgo-pojo/src/main/java/io/gridgo/utils/pojo/setter/data/ReferenceData.java
+++ commons/gridgo-pojo/src/main/java/io/gridgo/utils/pojo/setter/data/ReferenceData.java
@@ -1,14 +1,6 @@
 package io.gridgo.utils.pojo.setter.data;
 
-import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.function.Supplier;
-
-import io.gridgo.utils.pojo.PojoUtils;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-public interface ReferenceData extends KeyValueData {
+public interface ReferenceData extends GenericData {
 
     @Override
     default boolean isNull() {
@@ -31,51 +23,4 @@ public interface ReferenceData extends KeyValueData {
     default Object getInnerValue() {
         return this.getReference();
     }
-
-    @Override
-    default GenericData get(String key) {
-        return new SimpleReferenceData(PojoUtils.getValue(getReference(), key));
-    }
-
-    @Override
-    default GenericData getOrDefault(String key, GenericData def) {
-        var result = get(key);
-        return result == null ? def : result;
-    }
-
-    @Override
-    default GenericData getOrTake(String key, Supplier<GenericData> onAbsentSupplier) {
-        var result = get(key);
-        return result == null ? onAbsentSupplier.get() : result;
-    }
-
-    @Override
-    default Iterator<Entry<String, GenericData>> iterator() {
-//        var type = getReferenceClass();
-//        if (type == null)
-//            return null;
-//        var proxy = PojoGetterRegistry.DEFAULT.getGetterProxy(type);
-//        var count = new AtomicInteger(proxy.getFields().length);
-//        return new Iterator<Map.Entry<String, GenericData>>() {
-//
-//            @Override
-//            public Entry<String, GenericData> next() {
-//                return ;
-//            }
-//
-//            @Override
-//            public boolean hasNext() {
-//                return count.decrementAndGet() > 0;
-//            }
-//        };
-        return null;
-    }
-}
-
-@AllArgsConstructor
-class SimpleReferenceData implements ReferenceData {
-
-    @Getter
-    private final Object reference;
-
 }
