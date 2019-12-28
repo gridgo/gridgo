8a5f7db7954c7aec35653172a9329f7fd9b852a2
Anh Dung Bui <buidun@amazon.com>
2019-12-28 19:51:19 +0900

    remove unused exceptions

--- commons/gridgo-bean/src/main/java/io/gridgo/bean/exceptions/SerializationPluginException.java
+++ commons/gridgo-bean/src/main/java/io/gridgo/bean/exceptions/SerializationPluginException.java
@@ -4,19 +4,11 @@ public class SerializationPluginException extends RuntimeException {
 
     private static final long serialVersionUID = -5630203172743701283L;
 
-    public SerializationPluginException() {
-        super();
-    }
-
     public SerializationPluginException(String message) {
         super(message);
     }
 
     public SerializationPluginException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public SerializationPluginException(Throwable cause) {
-        super(cause);
     }
 }
