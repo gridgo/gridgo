a3f0029f2729189b5c31f531359548041a84d3e2
Anh Dung Bui <buidun@amazon.com>
2019-12-28 13:34:49 +0900

    add unit tests

--- commons/gridgo-pojo/src/test/java/io/gridgo/utils/pojo/setter/TestPojoSetter.java
+++ commons/gridgo-pojo/src/test/java/io/gridgo/utils/pojo/setter/TestPojoSetter.java
@@ -1,21 +1,31 @@
 package io.gridgo.utils.pojo.setter;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import io.gridgo.utils.pojo.setter.data.SimpleKeyValueData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class TestPojoSetter {
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SimplePojo {
+        private String name;
+    }
 
     @Data
     public static class PojoWithCollection {
@@ -25,6 +35,12 @@ public class TestPojoSetter {
         private Object nullPointer;
 
         private List<String> list;
+
+        private List<SimplePojo> pojoList;
+
+        private List<SimplePojo> pojoList2;
+
+        private List<List<String>> nestedList;
 
         private Set<String> set;
 
@@ -38,6 +54,9 @@ public class TestPojoSetter {
     @Test
     public void testPojoSetterCollection() {
         var list = List.of("s1", "s2", "s3");
+        var nestedList = List.of(List.of("s1", "s2"), List.of("s3"));
+        var pojoList = List.of(Map.of("name", "test1"), Map.of("name", "test2"));
+        var pojoList2 = List.of(SimplePojo.builder().name("test1").build());
         var set = Set.of("s5", "s4", "s6");
         var map = Map.of("k1", "v1", "k2", "v2");
         var intArr = new int[] { 3, 4, 5, 6 };
@@ -46,6 +65,9 @@ public class TestPojoSetter {
         var src = new HashMap<String, Object>();
         src.put("intValue", 1);
         src.put("list", list);
+        src.put("nestedList", nestedList);
+        src.put("pojoList", pojoList);
+        src.put("pojoList2", pojoList2);
         src.put("set", set);
         src.put("map", map);
         src.put("intArr", intArr);
@@ -57,10 +79,18 @@ public class TestPojoSetter {
 
         assertNull(obj.getNullPointer());
         assertEquals(1, obj.getIntValue());
-        assertTrue(list.equals(obj.getList()));
-        assertTrue(set.equals(obj.getSet()));
-        assertTrue(map.equals(obj.getMap()));
+        assertEquals(list, obj.getList());
+        assertEquals(nestedList, obj.getNestedList());
+        assertEquals(set, obj.getSet());
+        assertEquals(map, obj.getMap());
         assertArrayEquals(intArr, obj.getIntArr());
         assertArrayEquals(objArr, obj.getObjArr());
+
+        assertEquals(2, obj.getPojoList().size());
+        assertEquals("test1", obj.getPojoList().get(0).getName());
+        assertEquals("test2", obj.getPojoList().get(1).getName());
+
+        assertEquals(1, obj.getPojoList2().size());
+        assertEquals("test1", obj.getPojoList2().get(0).getName());
     }
 }
