bef076ecbe3f67f7d32c79069c7d69e600497e3b
Anh Dung Bui <buidun@amazon.com>
2019-12-31 18:55:43 +0900

    remove unused methods

--- connector/src/main/java/io/gridgo/connector/support/exceptions/InvalidPlaceholderException.java
+++ connector/src/main/java/io/gridgo/connector/support/exceptions/InvalidPlaceholderException.java
@@ -4,19 +4,7 @@ public class InvalidPlaceholderException extends RuntimeException {
 
     private static final long serialVersionUID = 1882627679265529060L;
 
-    public InvalidPlaceholderException() {
-        super();
-    }
-
     public InvalidPlaceholderException(String message) {
         super(message);
-    }
-
-    public InvalidPlaceholderException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidPlaceholderException(Throwable cause) {
-        super(cause);
     }
 }
